c77a6a31a06a405411a37ebb79235bdc6d2d723d
FM-84 <137740087+FM-84@users.noreply.github.com>
2024-04-13 21:25:24 +0800

    ShoppingCart.java

--- src/main/java/bookstore/ShoppingCart.java
+++ src/main/java/bookstore/ShoppingCart.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 public class ShoppingCart extends ArrayList<Book> {
 	private static final long serialVersionUID = 1L;
 	public ShoppingCart() {
+		super();
 	}
 	
 	public void addBook(Book book) {
 		//Put your code here
+		this.add(book);
 	}
 	
 	public Book getBook(int i) {
 		//Put your code here
+		return this.get(i);
 	}
 	
 	public double getTotalPrice() {
 		//Put your code here
+		double total = 0;
+		for (Book book : this) {
+            		total += book.getPrice();
+        	}
+        	return total;
 	}
 }
